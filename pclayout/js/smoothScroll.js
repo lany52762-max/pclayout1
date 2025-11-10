30cdcde9d23c60f949feb34c0fd9f48781b26cf0
lany52762-max <lany52762@gmail.com>
2025-11-10 10:17:35 +0300

    Add files via upload

---
+++ pclayout/js/smoothScroll.js
@@ -0,0 +1,20 @@
+const navbar = document.querySelector ('.header__nav')
+const links = navbar.querySelectorAll ('a')
+
+links.forEach ((link) => {
+    link.addEventListener ('click', (event) => {
+     event.preventDefault ()
+     console.log (seamless);
+
+     const section = document.querySelector (link.getAttribute('href'))
+
+     if (section) {
+        seamless.scrollIntoView(document.querySelector("#target"), {
+        behavior: "smooth",
+        block: "center",
+        inline: "center",
+    });
+     }
+    })
+})
+
